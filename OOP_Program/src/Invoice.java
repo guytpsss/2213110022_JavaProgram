4c9def00fd6909edccc3babf58468a81c2666bd4
guytpsss <phoodislucky6@gmail.com>
2023-01-26 16:31:07 +0700

    Pratice_2[26-Jan-2023]

---
+++ OOP_Program/src/Invoice.java
@@ -0,0 +1,45 @@
+
+public class Invoice {
+	private int id;
+	private Customer customer;
+	private double amount;
+	
+	public Invoice(int id,Customer customer,double amount) {
+		this.id = id;
+		this.customer = customer;
+		this.amount = amount;
+	}
+	public int getID() {
+		return id;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		 this.customer = customer;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public int getCustomerID() {
+		return customer.getID();
+	}
+	public String getCustomerName() {
+		return customer.getName();
+	}
+	public int getCustomerdiscount() {
+		return customer.getDiscount();
+	}
+	public double getAmountAfterDiscount() {
+		return amount - ((double)customer.getDiscount()/100*amount);
+	}
+	public String toString(){
+		return "Invoice[id="+id+
+				",customer="+customer.getName()+"("+customer.getID()+")"
+				+"(" + customer.getDiscount() + "%)," 
+				+ "amount=" + getAmountAfterDiscount() +"]";
+	}
+}
